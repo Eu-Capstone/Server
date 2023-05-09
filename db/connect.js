ab1e149496c51dce1c22032bbac306994240df23
wooseok123 <hosmimam@naver.com>
2023-05-10 02:19:07 +0900

    chore : add option

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   const uri = `mongodb+srv://ongdalwater:pwj2EEPPDfYsn96b@capstone.00k8dii.mongodb.net/?retryWrites=true&w=majority`;
-  await mongoose.connect(uri);
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 const disconnectDB = () => {
