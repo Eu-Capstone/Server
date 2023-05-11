86c42dfb53824d8d3eb259141cb0998dafe998ac
wooseok123 <hosmimam@naver.com>
2023-05-11 10:13:24 +0900

    feat : remove pool

--- db/connect.js
+++ db/connect.js
@@ -2,12 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const uri = `mongodb+srv://vercel-admin-user:fq1GXFlR15kSyUA0@capstone.00k8dii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-  await mongoose.connect(uri, {
+  const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    poolSize: 10,
-    minSize: 5,
-  });
+    serverSelectionTimeoutMS: 5000,
+  };
+
+  try {
+    const connection = await mongoose.createConnection(uri, options);
+    return connection;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const disconnectDB = () => {
